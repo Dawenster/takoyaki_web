23e772164a6dc098fc6a149e18f24c4a06fca3d0
David Wen <dstwen@gmail.com>
2014-12-16 08:57:41 -0800

    add phrases routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :phrases
+
   scope "/api" do
     get "game_details" => "apis#game_details", as: :game_details
     get "new_phrase" => "apis#new_phrase", as: :new_phrase
