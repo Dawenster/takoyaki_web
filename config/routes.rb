891b55fa3e5beb799ff5c55893f1a4fd0ea3ba4d
David Wen <dstwen@gmail.com>
2014-12-16 09:02:24 -0800

    add root path; change new_phrase to next_phrase

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root "phrases#index"
+
   resources :phrases
 
   scope "/api" do
     get "game_details" => "apis#game_details", as: :game_details
-    get "new_phrase" => "apis#new_phrase", as: :new_phrase
+    get "next_phrase" => "apis#next_phrase", as: :next_phrase
   end
 end
