7eefc459e919c7951d2a85ff9a2988ee15be0dcd
David Wen <dstwen@gmail.com>
2014-12-21 09:17:57 -0800

    add http auth

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,4 +1,6 @@
 class PhrasesController < ApplicationController
+  http_basic_authenticate_with :name => ENV['USERNAME'], :password => ENV['PASSWORD']
+
   def index
     @phrases = Phrase.all.order(:id)
   end
